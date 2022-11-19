ab0354f4cd036e221d0a75854bbe7fc4db9f2071
meishafifah <meishafifah@students.amikom.ac.id>
2022-11-19 13:25:49 +0700

    feat: add share link competition website

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -41,6 +41,15 @@
 
         gtag('config', 'G-93ZHXK6RDS');
     </script>
+    <!-- FB Share -->
+    <meta property="og:url" content="URLHERE" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="TITLE HERE" />
+    <meta property="og:description" content="DESC HERE" />
+    <meta property="og:image" content="URLIMAGE" />
+    <meta property="og:image:width" content="800" />
+    <meta property="og:image:height" content="600" />
+    <!-- FB Share -->
 </head>
 
 <body>
@@ -664,6 +673,11 @@
 
         <div class="mt-10">
             <div class="mb-5 bg-white p-6 rounded-3xl">
+                <div class="relative">
+                    <a class="absolute rounded-full button-color p-3 top-[-40px] right-[-40px]" target="_blank" href="whatsapp://send?text=Kuliah kebanyakan teori, saatnya ikut kompetisi!%0a%0aPernah menyaksikan Keseruan Hackathon pada Drakor Startup ?%0a%0aKabar baik nih, tidak lama lagi hadir di AMIKOM.  Kamu bisa merasakan Sensasi membangun Produk Digital dalam 5 Hari, melalui Hackathon.%0a%0aSelain itu, ini momentum yang tepat buat Kamu mewujudkan Ide briliant penerapan teknologi AI, AR/VR, IoT atau Animasi dalam rangkaian Kompetisi Produk.%0a%0aMenangkan Jutaan Rupiah, dan puluhan benefit lainnya dari Amazone Web Service dan Vibicloud.%0a%0aSayangnya waktu pendaftaran terbatas. Ketuk tautan sekarang juga.%0a%0ahttps://amicta.amikom.ac.id" data-action="share/whatsapp/share">
+                        <svg  style="width: 15px; fill: white;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M307 34.8c-11.5 5.1-19 16.6-19 29.2v64H176C78.8 128 0 206.8 0 304C0 417.3 81.5 467.9 100.2 478.1c2.5 1.4 5.3 1.9 8.1 1.9c10.9 0 19.7-8.9 19.7-19.7c0-7.5-4.3-14.4-9.8-19.5C108.8 431.9 96 414.4 96 384c0-53 43-96 96-96h96v64c0 12.6 7.4 24.1 19 29.2s25 3 34.4-5.4l160-144c6.7-6.1 10.6-14.7 10.6-23.8s-3.8-17.7-10.6-23.8l-160-144c-9.4-8.5-22.9-10.6-34.4-5.4z"/></svg>
+                    </a>        
+                </div>
                 <div class="flex flex-col sm:grid sm:grid-cols-6 gap-0.5 lg:gap-2 items-center">
                     <div class="w-16 lg:w-28 p-1 mb-2 sm:mb-0"><img src="{{ asset('assets/img/icon4.svg') }}"
                             alt=""></div>
@@ -681,6 +695,11 @@
                 </div>
             </div>
             <div class="mb-5 bg-white p-6 rounded-3xl">
+                <div class="relative">
+                    <a class="absolute rounded-full button-color p-3 top-[-40px] right-[-40px]" target="_blank" href="whatsapp://send?text=Kuliah kebanyakan teori, saatnya ikut kompetisi!%0a%0aPernah menyaksikan Keseruan Hackathon pada Drakor Startup ?%0a%0aKabar baik nih, tidak lama lagi hadir di AMIKOM.  Kamu bisa merasakan Sensasi membangun Produk Digital dalam 5 Hari, melalui Hackathon.%0a%0aSelain itu, ini momentum yang tepat buat Kamu mewujudkan Ide briliant penerapan teknologi AI, AR/VR, IoT atau Animasi dalam rangkaian Kompetisi Produk.%0a%0aMenangkan Jutaan Rupiah, dan puluhan benefit lainnya dari Amazone Web Service dan Vibicloud.%0a%0aSayangnya waktu pendaftaran terbatas. Ketuk tautan sekarang juga.%0a%0ahttps://amicta.amikom.ac.id" data-action="share/whatsapp/share">
+                        <svg  style="width: 15px; fill: white;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M307 34.8c-11.5 5.1-19 16.6-19 29.2v64H176C78.8 128 0 206.8 0 304C0 417.3 81.5 467.9 100.2 478.1c2.5 1.4 5.3 1.9 8.1 1.9c10.9 0 19.7-8.9 19.7-19.7c0-7.5-4.3-14.4-9.8-19.5C108.8 431.9 96 414.4 96 384c0-53 43-96 96-96h96v64c0 12.6 7.4 24.1 19 29.2s25 3 34.4-5.4l160-144c6.7-6.1 10.6-14.7 10.6-23.8s-3.8-17.7-10.6-23.8l-160-144c-9.4-8.5-22.9-10.6-34.4-5.4z"/></svg>
+                    </a>        
+                </div>
                 <div class="flex flex-col sm:grid sm:grid-cols-6 gap-0.5 lg:gap-2 items-center">
                     <div class="w-16 lg:w-28 p-1 mb-2 sm:mb-0"><img src="{{ asset('assets/img/icon5.svg') }}"
                             alt=""></div>
@@ -699,6 +718,11 @@
                 </div>
             </div>
             <div class="mb-5 bg-white p-6 rounded-3xl">
+                <div class="relative">
+                    <a class="absolute rounded-full button-color p-3 top-[-40px] right-[-40px]" target="_blank" href="whatsapp://send?text=Kuliah kebanyakan teori, saatnya ikut kompetisi!%0a%0aPernah menyaksikan Keseruan Hackathon pada Drakor Startup ?%0a%0aKabar baik nih, tidak lama lagi hadir di AMIKOM.  Kamu bisa merasakan Sensasi membangun Produk Digital dalam 5 Hari, melalui Hackathon.%0a%0aSelain itu, ini momentum yang tepat buat Kamu mewujudkan Ide briliant penerapan teknologi AI, AR/VR, IoT atau Animasi dalam rangkaian Kompetisi Produk.%0a%0aMenangkan Jutaan Rupiah, dan puluhan benefit lainnya dari Amazone Web Service dan Vibicloud.%0a%0aSayangnya waktu pendaftaran terbatas. Ketuk tautan sekarang juga.%0a%0ahttps://amicta.amikom.ac.id" data-action="share/whatsapp/share">
+                        <svg  style="width: 15px; fill: white;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M307 34.8c-11.5 5.1-19 16.6-19 29.2v64H176C78.8 128 0 206.8 0 304C0 417.3 81.5 467.9 100.2 478.1c2.5 1.4 5.3 1.9 8.1 1.9c10.9 0 19.7-8.9 19.7-19.7c0-7.5-4.3-14.4-9.8-19.5C108.8 431.9 96 414.4 96 384c0-53 43-96 96-96h96v64c0 12.6 7.4 24.1 19 29.2s25 3 34.4-5.4l160-144c6.7-6.1 10.6-14.7 10.6-23.8s-3.8-17.7-10.6-23.8l-160-144c-9.4-8.5-22.9-10.6-34.4-5.4z"/></svg>
+                    </a>        
+                </div>
                 <div class="flex flex-col sm:grid sm:grid-cols-6 gap-0.5 lg:gap-2 items-center">
                     <div class="w-16 lg:w-28 p-1 mb-2 sm:mb-0"><img src="{{ asset('assets/img/icon6.svg') }}"
                             alt=""></div>
