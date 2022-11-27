dafe4e3508156ccea8359eb9f2a20f57f7954ede
Muhammad Mufti Ashshiddiqi <muftishid@gmail.com>
2022-11-27 13:20:11 +0700

    feat: update export excel

--- app/Exports/SubmissionExport.php
+++ app/Exports/SubmissionExport.php
@@ -4,18 +4,38 @@ namespace App\Exports;
 
 use App\Models\Submission;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class SubmissionExport implements FromCollection
+class SubmissionExport implements FromCollection, WithHeadings
 {
     /**
      * @return \Illuminate\Support\Collection
      */
     public function collection()
     {
-        return Submission::join('users', 'submissions.user_id', '=', 'users.id')
-        ->join('competitions', 'submissions.competition_id', '=', 'competitions.id')
-        ->join('teams', 'submissions.team_id', '=', 'teams.id')
-        ->join('assignments', 'submissions.assignment_id', '=', 'assignments.id')
-        ->get();
+        $data = Submission::leftJoin('users', 'submissions.user_id', '=', 'users.id')
+        ->leftJoin('competitions', 'submissions.competition_id', '=', 'competitions.id')
+        ->leftJoin('teams', 'submissions.team_id', '=', 'teams.id')
+        ->leftJoin('assignments', 'submissions.assignment_id', '=', 'assignments.id')
+        ->get(['competitions.name as competitionname', 'assignments.title', 'users.nim', 'users.name', 'users.email', 'users.phone', 'teams.name as teamname', 'teams.product', 'teams.description', 'submissions.status', 'response']);
+
+        return $data;
+    }
+
+    public function headings(): array
+    {
+        return [
+            'COMPETITION',
+            'SUBMISSION',
+            'NIM',
+            'NAME',
+            'EMAIL',
+            'PHONE',
+            'TEAM',
+            'PRODUCT',
+            'DESCRIPTION',
+            'STATUS',
+            'RESPONSE'
+        ];
     }
 }
